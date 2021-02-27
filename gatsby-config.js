16cf87dc3b1d9fcfbbe5e54d3019e46912862e81
p11i <pouyahosseyni7@gmail.com>
2021-02-27 17:55:55 +0330

    added about

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: '/AmirArdroudi.github.io',
+  pathPrefix: '/home',
   siteMetadata: {
     title: 'Amir Ardroudi',
     description:
