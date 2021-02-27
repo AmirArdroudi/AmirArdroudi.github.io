ba5b0aab5b006126af4175b050255dd2a08dec9b
javepe6125 <pouyahosseyni7@gmail.com>
2021-02-27 16:03:58 +0330

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4150ce0cd11e82d6c35c.js"
+    "url": "webpack-runtime-c69905443e0dbc097f0e.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2c41b09f4eefbad92edf21b2a82691b7"
+    "revision": "7c567872cf8b07a471ace52befc9e352"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8033a24e61cb6e6f32a5.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e34d3fcbe0c429bfdf23c2bcd80ed00c"
+    "revision": "e2084e03912646b24abc627ff05637ca"
   },
   {
     "url": "polyfill-7e4d7804df080eb8f631.js"
